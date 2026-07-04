1bd9f3cd42af070dfab152f6f330001113e5a745
yungtry <yungtry@users.noreply.github.com>
2026-07-04 22:31:40 +0200

    Delete refactor.js

--- refactor.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const file = 'ps3-avatar-adder.user.js';
-let content = fs.readFileSync(file, 'utf8');
-
-// I will create a python script instead to generate the refactored script.
